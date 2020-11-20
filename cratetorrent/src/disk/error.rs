6658295c5b589aec810500ea6b56e919b6660741
Marc Loedi <marc@enhance.com>
2020-11-20 09:58:24 +0100

    Restructure disk IO module as it was getting too big

--- cratetorrent/src/disk/error.rs
+++ cratetorrent/src/disk/error.rs
@@ -5,27 +5,6 @@ use crate::error::Error;
 /// The disk IO result type.
 pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
 
-/// Error type returned on failed block writes.
-///
-/// This error is non-fatal so it should not be grouped with the global `Error`
-/// type as it may be recovered from.
-#[derive(Debug)]
-pub(crate) enum WriteError {
-    /// The block's piece index is invalid.
-    InvalidPieceIndex,
-    /// An IO error ocurred.
-    Io(std::io::Error),
-}
-
-impl fmt::Display for WriteError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::InvalidPieceIndex => write!(fmt, "invalid piece index"),
-            Self::Io(e) => write!(fmt, "{}", e),
-        }
-    }
-}
-
 /// Error type returned on failed torrent allocations.
 ///
 /// This error is non-fatal so it should not be grouped with the global `Error`
@@ -50,6 +29,27 @@ impl fmt::Display for NewTorrentError {
             Self::AlreadyExists => {
                 write!(fmt, "disk torrent entry already exists")
             }
+            Self::Io(e) => write!(fmt, "{}", e),
+        }
+    }
+}
+
+/// Error type returned on failed block writes.
+///
+/// This error is non-fatal so it should not be grouped with the global `Error`
+/// type as it may be recovered from.
+#[derive(Debug)]
+pub(crate) enum WriteError {
+    /// The block's piece index is invalid.
+    InvalidPieceIndex,
+    /// An IO error ocurred.
+    Io(std::io::Error),
+}
+
+impl fmt::Display for WriteError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::InvalidPieceIndex => write!(fmt, "invalid piece index"),
             Self::Io(e) => write!(fmt, "{}", e),
         }
     }
