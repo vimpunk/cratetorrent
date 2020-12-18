d1bcfffe984bece29232f0c9422f55fb7c6a8c71
mandreyel <mandreyel@protonmail.com>
2020-12-18 15:19:37 +0100

    Fix one inconsistent import

--- cratetorrent/src/disk/io.rs
+++ cratetorrent/src/disk/io.rs
@@ -4,8 +4,6 @@ pub(crate) mod torrent;
 
 #[cfg(test)]
 mod tests {
-    use sha1::{Digest, Sha1};
-
     use std::{
         collections::BTreeMap,
         fs,
@@ -14,6 +12,8 @@ mod tests {
         path::{Path, PathBuf},
         sync,
     };
+
+    use sha1::{Digest, Sha1};
 
     use crate::{
         disk::{
